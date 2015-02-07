990720a95b4eba9d0dfb989d686a143a8424b7c3
Darin Reid <darinreid@gmail.com>
2015-02-07 10:19:06 -0800

    Initial commit

---
+++ lib/WpDotConfig/Configurator.php
@@ -0,0 +1,10 @@
+<?php namespace WpDotConfig;
+
+class Configurator {
+
+    public function __construct()
+    {
+        
+    }
+
+}
---
+++ wp-dot-config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name:     WP Dot Config
+ * Description:     Easy configuration for professional WordPress themes.
+ * Version:         1.0.0-dev
+ * Author:          Darin Reid / Elegant Contraption
+ * Author URI:      http://elcontraption.com/
+ */
+
+if ( ! defined('WP_DOT_CONFIG_PATH')) 
+{
+    define('WP_DOT_CONFIG_PATH', plugin_dir_path(__FILE__));
+}
+
+require WP_DOT_CONFIG_PATH . 'vendor/autoload.php';
+
+add_action('plugins_loaded', function() 
+{
+    new WpDotConfig\Configurator();
+});
