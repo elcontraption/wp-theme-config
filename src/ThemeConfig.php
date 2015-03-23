a9ae482876136d3eb500da8ceb5ff86eafdfb111
Darin Reid <darinreid@gmail.com>
2015-03-23 14:49:43 -0700

    Adding static instance method

--- src/ThemeConfig.php
+++ src/ThemeConfig.php
@@ -2,9 +2,31 @@
 
 class ThemeConfig {
 
-    public function __construct ()
+    /**
+     * Instance of this class
+     */
+    protected static $instance = null;
+
+    /**
+     * Constructor
+     */
+    protected function __construct()
     {
-        
+
+    }
+
+    /**
+     * Return an instance of this class
+     */
+    public static function getInstance()
+    {
+        // If the single instance hasn't been set, set it now.
+        if (self::$instance == null)
+        {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
     }
 
 }
